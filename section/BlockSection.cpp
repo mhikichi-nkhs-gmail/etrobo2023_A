85c6af216b7e64bf4787d880a0889f6d0ca50db4
lohagil <torimaruitou@gmail.com>
2023-08-31 18:05:23 +0900

    BlockSection途中経過

--- section/BlockSection.cpp
+++ section/BlockSection.cpp
@@ -8,110 +8,75 @@ BlockSection::BlockSection()
 }
 
 bool BlockSection::run()
-{
-    if(point==0)
+{   
+    switch(now_position)
     {
-        switch(lno)
-        {
-            case SET_L_PATTERN0:
-                set();
-                lno = L_PATTERN0;
-                return false;
-                break;
-            case L_PATTERN0:
-                set();
-                lno = SET_L_PATTERN1;
-                return false;
-                break;
-            case SET_L_PATTERN1:
-                set();
-                lno = L_PATTERN1;
-                return false;
-                break;
-            case L_PATTERN1:
-                set();
-                lno = SET_L_PATTERN2;
-                return false;
-                break;
-            case SET_L_PATTERN2:
-                set();
-                lno = L_PATTERN2;
-                return false;
-                break;
-            case L_PATTERN2:
-                set();
-                lno = SET_L_PATTERN3;
-                return false;
-                break;
-            case SET_L_PATTERN3:
-                set();
-                lno = L_PATTERN3;
-                return false;
-                break;
-            case L_PATTERN3:
-                set();
-                return true;
-        }
-        
+    case -1:
+        set(In_Block);
+        now_position=position[pattern][0];
+        break;
+    case red_position:
+        break;
+    case blue_position1:
+        break;
+    case blue_position2:
+        break;
+    default:
+        break;
     }
-    else
-    {
-        switch(rno)
-        {
-            case SET_R_PATTERN0:
-                set();
-                rno = R_PATTERN0;
-                return false;
-                break;
-            case R_PATTERN0:
-                set();
-                rno = SET_R_PATTERN1;
-                return false;
-                break;
-            case SET_R_PATTERN1:
-                set();
-                rno = R_PATTERN1;
-                return false;
-                break;
-            case R_PATTERN1:
-                set();
-                rno = SET_R_PATTERN2;
-                return false;
-                break;
-            case SET_R_PATTERN2:
-                set();
-                rno = R_PATTERN2;
-                return false;
-                break;
-            case R_PATTERN2:
-                set();
-                rno = SET_R_PATTERN3;
-                return false;
-                break;
-            case SET_R_PATTERN3:
-                set();
-                rno = R_PATTERN3;
-                return false;
-                break;
-            case R_PATTERN3:
-                set();
-                return true;
-        }
-        return false;
-    }
+    return false;
 }
 
 void BlockSection::course(int direct)
 {
-    printf("Block course\n");
     if(direct == 0)
     {
-        printf("Block L\n");
-        lno=SET_L_PATTERN0;
+        course_flag = 1;
     }
     else
     {
-        printf("Block R\n");
-        rno=SET_R_PATTERN0;
+        course_flag = -1;
     }
 }
+
+void BlockSection::next_circle(double TA,double H,double S,enum Edge_Pattern)
+{
+    turn_angle=TA;
+    h=H;
+    s=S;
+    if(TA==0)
+    {
+        in_length=2.5;
+        out_length=2.5;
+    }
+    else
+    {
+        switch(position_edge[pattern][now_position])
+        {
+            case 1://rr
+                in_length=2.5;
+                out_length=2.5;
+                break;
+            case 2//rl
+                in_length=2.5;
+                out_length=2.5;
+                break;
+            case -2//lr
+                in_length=2.5;
+                out_length=2.5;
+                break;
+            case -1//ll
+                in_length=2.5;
+                out_length=2.5;
+                break;
+        }
+    }
+    if(SectionManager::run())
+    {
+        set(Next_Circle);
+        count++;
+        now_position=position[pattern][count];
+        
+    }
+}
+
--- section/BlockSection.h
+++ section/BlockSection.h
@@ -1,81 +1,80 @@
 #ifndef _BLOCK_SECTION_H_
 #define _BLOCK_SECTION_H_
-
 #include "SectionManager.h"
-class Block_Section : public SectionManager
-{
+#include "HackEv3.h"
+
+class BlockSection : public SectionManager  
+{  
     public:
+
         BlockSection();
         bool run();
         void course(int direct);
+        void next_circle(double TA,double H,double S,enum Edge_Pattern);
+
+
+
     protected:
+        COURSNO NO;
+
+#if defined(MAKE_RIGHT)
+    const int _EDGE = LineTracer::LEFTEDGE;
+    const int _OPPOSITION_EDGE = LineTracer::RIGHTEDGE;
+#else
+    const int _EDGE = LineTracer::RIGHTEDGE;
+    const int _OPPOSITION_EDGE = LineTracer::LEFTEDGE;
+#endif
+
+
     private:
-        int point;
-        
-        enum L_BLOCK_NO{
-            SET_L_PATTERN0,
-            L_PATTERN0,
-            SET_L_PATTERN1,
-            L_PATTERN1,
-            SET_L_PATTERN2,
-            L_PATTERN2
-            SET_L_PATTERN3,
-            L_PATTERN3
-        };
+        int now_position = -1;
+        int red_position;
+        int blue_position1;
+        int blue_position2;
+        int course_flag;
+        int count=0;
+        int pattern;
+        int position[][] =
+                            {
+                             {13,9,5,1,2,3,4,8,12,16,15,14,10,6,7,11},
+                             {13,14,15,16,12,8,4,3,2,1,5,9,10,11,7,6},
+                             {13,9,5,1,2,6,10,14,15,11,7,3,4,8,12,16},
+                             {13,14,15,16,12,11,10,9,5,6,7,8,4,3,2,1}
+                            };
+        double position_angle[][] =
+                            {
+                             {-90,0,0,-90,0,-90,-90,0,0,0,0,0,90,-90,0,-90},
+                             {90,0,0,90,0,0,90,0,90,0,90,0,0,0,0,90},
+                             {-90,-90,-90-90,0,0,0,0,0,0,0,0,90,90,90,0},
+                             {0,0,0,90,-90,0,0,90,-90,0,0,90,0,0,0,90},
+                            };
+        int position_edge[4][16];
+        double h;
+        double s;
+        double turn_angle;
+        double in_length;
+        double out_length;
 
-        enum R_BLOCK_NO{
-            SET_R_PATTERN0,
-            R_PATTERN0,
-            SET_R_PATTERN1,
-            R_PATTERN1,
-            SET_R_PATTERN2,
-            R_PATTERN2
-            SET_R_PATTERN3,
-            R_PATTERN3
-        };
+        SecParam In_Block[15] = {
+            {Section::WALKER,{0,0},Section::TIME,{10, Section::FLNONE}, Section::EMNONE,{}},
+            {Section::VIRSLINE,{55, -25, 0, 28, 2.5 ,1.2},Section::LENGTH,{2.5, Section::RESETLEN}, Section::EMNONE,{}},
+            
+            
+            {Section::TWALKER,{-90},Section::TURNANGLE,{75, Section::RESETANG}, Section::EMNONE,{}},
+            {Section::VIRSLINE,{-55, 0, 0, 25, 2.5 ,2.0},Section::LENGTH,{-15, Section::RESETLEN}, Section::EMNONE,{}},
+            {Section::WALKER,{0,0},Section::TIME,{300, Section::FLNONE}, Section::EMNONE,{}},
+            {Section::VIRSLINE,{55, 0, 0, 25, 2.5 ,2.0},Section::LENGTH,{15, Section::RESETLEN}, Section::EMNONE,{}},
+            {Section::TWALKER,{60},Section::TURNANGLE,{-75, Section::RESETANG}, Section::EMNONE,{}},
+            {Section::WALKER,{0,0},Section::TIME,{1, Section::FLNONE}, Section::EMNONE,{}},
+            {Section::WANONE,{},Section::JUNONE,{}, Section::EMNONE,{}}};
 
-        L_BLOCK_NO lno;
-        R_BLOCK_NO rno;
+        SecParam Next_Circle[5]={
+            {Section::VIRSLINE,{55, 0, 0, 28, 2.5 ,1.2},Section::LENGTH,{in_length, Section::RESETLEN}, Section::EMNONE,{}},
+            {Section::TWALKER,{60,0},Section::TURNANGLE,{turn_angle,Section::RESETANG},Section::EMNONE,{}},
+            {Section::VIRSLINE,{55, 0, 0, 28, 2.5 ,1.2},Section::LENGTH,{out_length, Section::RESETLEN}, Section::EMNONE,{}},
+            {Section::TRACER,{60, 0.05, 48, 2.5, 5.0 ,0 , 0, _EDGE},Section::COLOR,{h,s, Section::RESETLEN}, Section::EMNONE,{}},
+            {Section::WANONE,{},Section::JUNONE,{}, Section::EMNONE,{}}};
 
-        SecParam LP0[100] = 
-        { 
-
-        };
-
-        SecParam LP1[100] = 
-        { 
-
-        };
-
-        SecParam LP2[100] = 
-        { 
-
-        };
-
-        SecParam LP3[100] = 
-        { 
-
-        };
-
-        SecParam RP0[100] = 
-        { 
-
-        };
-
-        SecParam RP1[100] = 
-        { 
-
-        };
-
-        SecParam RP2[100] = 
-        { 
-
-        };
-
-        SecParam RP3[100] = 
-        { 
-
-        };
         
 };
 #endif
