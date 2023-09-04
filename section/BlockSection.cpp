b98984ffbb8e278b5f6a9798c31c4deba7905b39
HC41-1045 <nkhshc411045@g.neec.ac.jp>
2023-09-04 18:24:05 +0900

    ブロックテスト

--- section/BlockSection.cpp
+++ section/BlockSection.cpp
@@ -8,18 +8,61 @@ BlockSection::BlockSection()
 
 bool BlockSection::run()
 {
-    return true;
+    switch(COURSE)
+    {
+    case INIT_FIRST_LCOURSE:
+        set(Lfirst);
+        COURSE = FIRST_LCOURSE;
+        break;
+    case FIRST_LCOURSE:
+        change(num1);
+        break; 
+    case INIT_FIRST_RCOURSE:
+        set(Rfirst);
+        COURSE = FIRST_RCOURSE;
+        break;
+    case FIRST_RCOURSE:
+        change(num1);
+        break;
+    case INIT_BLUE_MARKER:
+        set(Go_blue_sircle);
+        break;
+    case BLUE_MARKER:
+        change(1);
+        break;
+    case END:
+        printf("END\n");
+        return true;
+    }
+    return false;
 }
 
 void BlockSection::course(int direct)
 {
     if(direct == 0)
     {
-        set(Lfirst);
+        COURSE = INIT_FIRST_LCOURSE;
+        num1 = 0;
     }
     else
     {
-        set(Rfirst);
+        COURSE = INIT_FIRST_RCOURSE;
+        num1 = 0;
+    }
+}
+
+void BlockSection::change(int num2)
+{
+    if(SectionManager::run())
+    {
+        switch(num2){
+            case 0:
+            COURSE = INIT_BLUE_MARKER;
+            break;
+            case 1:
+            COURSE = END;
+            break;
+        }
     }
 }
 
--- section/BlockSection.h
+++ section/BlockSection.h
@@ -8,6 +8,7 @@ class BlockSection : public SectionManager
         BlockSection();
         bool run();
         void course(int direct);
+        void change(int num2);
 
 
         enum BLOCK{
@@ -15,20 +16,35 @@ class BlockSection : public SectionManager
             FIRST_LCOURSE,
             INIT_FIRST_RCOURSE,
             FIRST_RCOURSE,
+            INIT_BLUE_MARKER,
+            BLUE_MARKER,
+            INIT_RED_MARKER,
+            RED_MARKER,
+            INIT_YELLOW_MARKER,
+            YELLOW_MARKER,
+            INIT_GRREN_MARKER,
+            GRREN_MARKER,
             END
         };
 
     protected:
 
+#if defined(MAKE_RIGHT)
+    const int _EDGE = LineTracer::LEFTEDGE;
+#else
+    const int _EDGE = LineTracer::RIGHTEDGE;
+#endif
 
         BLOCK COURSE;
 
     private:
+        int num1;
 
         SecParam Lfirst[10] = {
             {Section::TWALKER,{60},Section::TURNANGLE,{-75, Section::RESETANG}, Section::EMNONE,{}},
             {Section::VIRSLINE,{50, -75, 0, 28, 2.5 ,5.0,0},Section::LENGTH,{50, Section::RESETLEN}, Section::EMNONE,{}},
             {Section::VIRSLINE,{50, 75, 0, 28, 2.5 ,5.0,0},Section::BRIGHTNESS,{-1, Section::RESETLEN}, Section::EMNONE,{}},
+            {Section::TWALKER,{-60},Section::TURNANGLE,{0, Section::FLNONE}, Section::EMNONE,{}},
             {Section::WANONE,{},Section::JUNONE,{}, Section::EMNONE,{}}
         };
 
@@ -36,35 +52,37 @@ class BlockSection : public SectionManager
             {Section::TWALKER,{-60},Section::TURNANGLE,{75, Section::RESETANG}, Section::EMNONE,{}},
             {Section::VIRSLINE,{50, 75, 0, 28, 2.5 ,5.0,0},Section::LENGTH,{50, Section::RESETLEN}, Section::EMNONE,{}},
             {Section::VIRSLINE,{50, 75, 0, 28, 2.5 ,5.0,0},Section::BRIGHTNESS,{-1, Section::RESETLEN}, Section::EMNONE,{}},
+            {Section::TWALKER,{60},Section::TURNANGLE,{0, Section::FLNONE}, Section::EMNONE,{}},
             {Section::WANONE,{},Section::JUNONE,{}, Section::EMNONE,{}}
         };
-
-        /*
 
         SecParam Go_red_sircle[10] = {
             {Section::TRACER,{50, 0.05, 48, 2.5, 5.0 ,0 , 0, _EDGE},Section::LENGTH,{20, Section::RESETLEN}, Section::EMNONE,{}},
             {Section::TRACER,{50, 0.05, 48, 2.5, 5.0 ,0 , 0, _EDGE},Section::COLOR,{350,0.2, Section::FLNONE}, Section::EMNONE,{}},
+            {Section::WALKER,{0,0},Section::TIME,{1000, Section::FLNONE}, Section::EMNONE,{}},
             {Section::WANONE,{},Section::JUNONE,{}, Section::EMNONE,{}}
         };
 
         SecParam Go_blue_sircle[10] = {
             {Section::TRACER,{50, 0.05, 48, 2.5, 5.0 ,0 , 0, _EDGE},Section::LENGTH,{20, Section::RESETLEN}, Section::EMNONE,{}},
             {Section::TRACER,{50, 0.05, 48, 2.5, 5.0 ,0 , 0, _EDGE},Section::COLOR,{220,0.2, Section::FLNONE}, Section::EMNONE,{}},
+            //{Section::WALKER,{0,0},Section::TIME,{1000, Section::FLNONE}, Section::EMNONE,{}},
             {Section::WANONE,{},Section::JUNONE,{}, Section::EMNONE,{}}
         };
 
         SecParam Go_yellow_sircle[10] = {
             {Section::TRACER,{50, 0.05, 48, 2.5, 5.0 ,0 , 0, _EDGE},Section::LENGTH,{20, Section::RESETLEN}, Section::EMNONE,{}},
             {Section::TRACER,{50, 0.05, 48, 2.5, 5.0 ,0 , 0, _EDGE},Section::COLOR,{30,0.2, Section::FLNONE}, Section::EMNONE,{}},
+            //{Section::WALKER,{0,0},Section::TIME,{1000, Section::FLNONE}, Section::EMNONE,{}},
             {Section::WANONE,{},Section::JUNONE,{}, Section::EMNONE,{}}
         };
 
         SecParam Go_green_sircle[10] = {
             {Section::TRACER,{50, 0.05, 48, 2.5, 5.0 ,0 , 0, _EDGE},Section::LENGTH,{20, Section::RESETLEN}, Section::EMNONE,{}},
             {Section::TRACER,{50, 0.05, 48, 2.5, 5.0 ,0 , 0, _EDGE},Section::COLOR,{150,0.2, Section::FLNONE}, Section::EMNONE,{}},
+            //{Section::WALKER,{0,0},Section::TIME,{1000, Section::FLNONE}, Section::EMNONE,{}},
             {Section::WANONE,{},Section::JUNONE,{}, Section::EMNONE,{}}
         };
-        */
 
 };
 #endif
